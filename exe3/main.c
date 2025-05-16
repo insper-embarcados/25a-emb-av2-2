6c5d66b3480780abf32ae8888e627454d0b865d8
danielmpinto <danielmessias.engenharia@gmail.com>
2025-05-16 19:50:24 -0300

    salvando progresso

--- exe3/main.c
+++ exe3/main.c
@@ -24,6 +24,14 @@ typedef struct input {
 } input_t;
 
 QueueHandle_t xQueueInput;
+QueueHandle_t xQueueLed1;
+QueueHandle_t xQueueLed2;
+
+
+
+SemaphoreHandle_t xSemaphoreLed2;
+
+
 
 /**
  * NÃO MEXER!
@@ -48,6 +56,35 @@ void input_task(void* p) {
  * Seu código vem aqui!
  */
 
+ void main_task(void* p){
+
+    input_t dados;
+    while(1){ 
+        // direciona pacotes para as tasks certas
+        if(xQueueReceive(xQueueInput, &dados, pdMS_TO_TICKS(100))){
+            // envia dados de cada led para cada task
+            xQueueSend(xQueueLed1, &dados.num_led1,0);
+            xQueueSend(xQueueLed2, &dados.num_led2,0);
+        }
+    }
+ }
+
+void led_1_task(void* p){
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+    int pisca_vezes = 0;
+    while(1){
+        if(xQueueReceive(xQueueLed1, &pisca_vezes, pdMS_TO_TICKS(100))){
+            for(int i = 0; i<= pisca_vezes; i++){
+                gpio_put(LED_PIN_Y, 1);
+                vTaskDelay(pdMS_TO_TICKS(250));
+                gpio_put(LED_PIN_Y, 0);
+                vTaskDelay(pdMS_TO_TICKS(250));
+            }
+            // envia semaforo pro outro piscar
+        }
+    }
+}
 
 
 int main() {
@@ -57,7 +94,11 @@ int main() {
      * manter essas duas linhas!
      */
     xQueueInput = xQueueCreate(32, sizeof(input_t));
+    xQueueLed1 = xQueueCreate(32, sizeof(input_t));
+    xQueueLed2 = xQueueCreate(32, sizeof(input_t));
+    xSemaphoreLed2 = xSemaphoreCreateBinary();
     xTaskCreate(input_task, "Input", 256, NULL, 1, NULL);
+    xTaskCreate(main_task, "Input", 256, NULL, 1, NULL);
 
     /**
      * Seu código vem aqui!
