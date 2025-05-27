b4195a9f5e6ab370f2445f6d8163e2436f945bfd
danielmpinto <danielmessias.engenharia@gmail.com>
2025-05-27 19:20:26 -0300

    exe 1?

--- exe1/main.c
+++ exe1/main.c
@@ -1,87 +1,89 @@
-#include "pico/stdlib.h"
-#include <stdio.h>
-#include "hardware/gpio.h"
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-
-const int pins[5] = {2,3,4,5,6};
-const int btn = 22;
-const int swit = 28;
-volatile int swit_crementa = 0;
-volatile int btn_verde = 0;
-
-
-void btn_callback(uint gpio, uint32_t events) {
-    if (gpio == swit){ 
-        if(events == 0x4){
-            swit_crementa = 1;
-        }
-        else if (events == 0x8){
-            swit_crementa = 0;
-        }
-    }
-    if(gpio == btn && events == 0x4){
-        btn_verde = 1;
-    }
-
-  }
-
-
-// finalizado inicializacao
-void bar_init(){
-    for(int i = 0; i<5; i++){
-        gpio_init(pins[i]);
-        gpio_set_dir(pins[i], GPIO_OUT);
-        gpio_put(pins[i], 0);
-    }
-    gpio_init(btn);
-    gpio_set_dir(btn, GPIO_IN);
-    gpio_pull_up(btn);
-    gpio_init(swit);
-    gpio_set_dir(swit, GPIO_IN);
-    gpio_pull_up(swit);
-}
-
-// acende d=ou desliga leds
-void bar_display(int val){
-    for(int i = 0; i< val; i++){
-        gpio_put(pins[i], 1);
-    }
-
-    for(int j = 5; j> val; j-- ){
-        gpio_put(pins[j-1], 0);
-    }
-
-}
-
-int main() {
-    stdio_init_all();
-    bar_init();
-    gpio_set_irq_enabled_with_callback(btn, 
-        GPIO_IRQ_EDGE_FALL,
-        true,
-        &btn_callback);
-    gpio_set_irq_enabled(swit, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-    int counter = 0;
-    while (true) {
-        if(btn_verde){
-            if(swit_crementa ){
-                if ((counter + 1) <=5)counter++;
-                printf("switch incrementando\n");
-                printf("counter is %d", counter);
-                bar_display(counter);
-
-                
-            } else if (!swit_crementa){
-                if ((counter - 1) >=0)counter--;
-                printf("switch decrementando\n");
-                printf("counter is %d", counter);
-                bar_display(counter);
-              
-
-            }
-            btn_verde = 0;
-        }
-    }
-
-    
-}
+ #include <stdio.h>
+ #include "pico/stdlib.h"
+ #include "hardware/adc.h"
+ 
+ const int buzzer = 12;
+ 
+ volatile int g_timer_0 = 0;
+ 
+ bool timer_0_callback(repeating_timer_t *rt) {
+     g_timer_0 = 1;
+     return true; // keep repeating
+ }
+ 
+ 
+ // Baremetal  -- ok
+ // Timer  -- ok
+ // ADC   -- ok
+ // IRQ
+ 
+ 
+ // modifica frequencia de acordo com potenciometro
+ // timer de acordo com valor de potenciometro
+ 
+ 
+ 
+ 
+ 
+ void buzzer_tone(int freq, int duration_ms) {
+     gpio_init(buzzer);
+     gpio_set_dir(buzzer, GPIO_OUT);
+     gpio_put(buzzer,0);
+     int half_period_us = 1000000 / (2 * freq); // usar no timer
+     int cycles = (duration_ms * 1000) / (2 * half_period_us); // fixo
+     repeating_timer_t timer_0;
+     int timer_0_hz = 5;
+     int counter = 0;
+     // cria timer
+     if (!add_repeating_timer_us(half_period_us / timer_0_hz, 
+                                           timer_0_callback,
+                                           NULL, 
+                                           &timer_0)) {
+                   printf("Failed to add timer\n");
+           }
+     while (counter < cycles){
+       if(g_timer_0){
+         gpio_put(buzzer, !gpio_get(buzzer));
+         counter ++;
+         g_timer_0 = 0;
+       }
+     }
+     cancel_repeating_timer(&timer_0);
+ 
+     }
+ 
+ 
+ int main() {
+     stdio_init_all();
+     
+ 
+     adc_init();
+ 
+     // Make sure GPIO is high-impedance, no pullups etc
+     adc_gpio_init(26);
+     adc_select_input(0);
+     uint16_t result = 0;
+     const float conversion_factor = 3.3f / (1 << 12);
+     while(1){
+       result = adc_read();
+       if(result > 0){
+ 
+ 
+ 
+       //0v  ->  0hz
+       //3,3 v -> 3,300 hz
+ 
+       uint16_t result = adc_read();
+       // printf("%f \n", result * conversion_factor);
+       buzzer_tone((int)result * conversion_factor * 1000,50);
+ 
+       }
+           
+     }
+ }
